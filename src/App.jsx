1553164a0a188ff723a97b9e309ab934025dc1a3
Fahad Alam <fahadalam12405@gmail.com>
2025-06-08 00:12:30 +0500

    Update index.html for improved SEO and change favicon; comment out Contact component in App

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
               <Testimonial />
               <BannerSection />
               <FAQ />
-              <Contact/>
+              {/* <Contact/> */}
             </>
           }
         />
